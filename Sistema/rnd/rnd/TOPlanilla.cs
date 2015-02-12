46e2b4c07252e93b15fe11a234f567ed0e1d79f8
tb8988 <tb8988@gmail.com>
2015-02-12 12:18:18 -0600

    Get & Set
    todas las clases han sido creadas y todos los get y set

---
+++ Sistema/rnd/rnd/TOPlanilla.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace rnd
+{
+    public class TOPlanilla
+    {
+        private TOUsuario usuario;
+
+        public TOUsuario Usuario
+        {
+            get { return usuario; }
+            set { usuario = value; }
+        }
+        private TOTrabajdor trabjador;
+
+        public TOTrabajdor Trabajador
+        {
+            get { return trabjador; }
+            set { trabjador = value; }
+        }
+        private TOCalculos calculos;
+
+        public TOCalculos Calculos
+        {
+            get { return calculos; }
+            set { calculos = value; }
+        }
+        private TOPrestamo prestamo;
+
+        public TOPrestamo Prestamo
+        {
+            get { return prestamo; }
+            set { prestamo = value; }
+        }
+        private TOAhorro ahorro;
+
+        public TOAhorro Ahorro
+        {
+            get { return ahorro; }
+            set { ahorro = value; }
+        }
+       
+    }
+}
