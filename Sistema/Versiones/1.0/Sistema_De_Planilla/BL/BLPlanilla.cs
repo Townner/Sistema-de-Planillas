e57957a48878d3163ca7fe40d6a5b78a28700847
JoseSoto94 <pablosoto94@gmail.com>
2015-02-17 17:10:13 -0600

    Update

---
+++ Sistema/Versiones/1.0/Sistema_De_Planilla/BL/BLPlanilla.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using rnd.TOPlanilla;
+
+namespace rnd
+{
+    public class BLPlanilla
+    {
+        public void AgregarPlanilla(TOUsuario usuario, TOTrabajdor trabajador
+            , TOCalculos calculos, TOPrestamo prestamo, TOAhorro ahorro)
+        {
+            TOPlanilla pln = new TOPlanilla();
+            pln.Usuario = usuario;
+            pln.Trabajador = trabajador;
+            pln.Calculos = calculos;
+            pln.Prestamo = prestamo;
+            pln.Ahorro = ahorro;
+
+            DAPlanilla apln = new DAPlanilla();
+            apln.AgregarPlanilla(pln);
+        }
+
+        public void EditarPlanilla(TOPlanilla planilla)
+        {
+            DAPlanilla edapln = new DAPlanilla();
+            edapln.EditarPlanilla(planilla);
+        }
+
+        public TOPrestamo ConsultarPlanilla(TOPlanilla planilla)
+        {
+            DAPlanilla conapln = new DAPlanilla();
+            conapln.ConsultarPlanilla(planilla);
+            throw new NotImplementedException();
+        }
+
+        public void EliminarPlanilla(TOPlanilla planilla)
+        {
+            DAPlanilla elapln = new DAPlanilla();
+            elapln.EliminarPlanilla(planilla);
+        }
+    }
+}
---
+++ Sistema/Versiones/1.0/Sistema_De_Planilla/DA/DATrabajador.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using rnd.TOTrabajdor;
+
+namespace rnd
+{
+    public class DATrabajador
+    {
+
+        public void AgregarTrabajdor(TOTrabajdor trab)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ModifcarTrabajador(TOTrabajdor trab)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void EliminarTrabajador(TOTrabajdor trab)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ConsultarTrabajador(TOTrabajdor trab)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
